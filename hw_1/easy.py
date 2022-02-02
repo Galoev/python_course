230cf4e07b111ad5162aa2c76f2dc00166d42e5e
Galoev <galoyev.ilkin@gmail.com>
2022-02-02 23:28:13 +0300

    fix hw_1 easy task

--- hw_1/easy.py
+++ hw_1/easy.py
@@ -1,16 +1,10 @@
 def fibonacci(n: int) -> list:
     if n <= 0:
         return [0]
-
-    a = 0
-    b = 1
     res =[0, 1]
 
     for i in range(2, n + 1):
-        tmp = b
-        b += a
-        a = tmp
-        res.append(b)
+        res.append(res[-1] + res[-2])
     
     return res
 
