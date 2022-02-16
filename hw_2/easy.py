8ca6a5698777b0a0617d9a56e9a2f3a340014e4b
Galoev <galoyev.ilkin@gmail.com>
2022-02-17 00:32:03 +0300

    hw_2 easy task made in a more more functional style

--- hw_2/easy.py
+++ hw_2/easy.py
@@ -1,19 +1,17 @@
 from functools import reduce
 
+def generate_table_row(data):
+    return ' & '.join(map(str, data)) + "\\\\" +  " \\hline "
 
 def generate_table_head(cols):
     return " {" + reduce(lambda el_prev, el: el_prev + "c|", range(cols), "|") + "} "
 
 def generate_table(data, rows, cols):
-    result = "\\begin{tabular} " + generate_table_head(cols)
-    
-    result += "\\hline "
-    for i in range(rows):
-        result += ' & '.join(map(str, data[i]))
-        result += "\\\\"
-        result += " \\hline "
-
-    return result + "\\end{tabular}"
+    return "\\begin{tabular} " + \
+            generate_table_head(cols) + \
+            "\\hline " + \
+            " ".join([generate_table_row(data[i]) for i in range(rows)]) + \
+            "\\end{tabular}"
 
 def generate_latex(table):
     return "\\documentclass{article}\\usepackage[utf8]{inputenc}\\begin{document}" + \
